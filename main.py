e5d42b7d48270d2dae89f210baa7d73e52d67047
JosephSkapin <joskapin@gmail.com>
2024-12-05 00:06:57 -0500

    Hello

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello")
+from manim import *
+
+class Test(Scene):
+    def construct(self):
+        sq = Square(side_length=5, stroke_color=GREEN, fill_color=PINK,fill_opacity=.75)
+        self.play(Create(sq), run_time = 3)
+        self.wait()
